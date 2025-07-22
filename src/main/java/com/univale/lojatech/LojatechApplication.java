1bde37499fc01c8d3bb3f3ff992f7d5e55738f05
Matheus Ribeiro Ávila <ribeiroavilamatheus@gmail.com>
2025-07-21 22:55:53 -0300

    First commit

---
+++ src/main/java/com/univale/lojatech/LojatechApplication.java
@@ -0,0 +1,13 @@
+package com.univale.lojatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojatechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojatechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/univale/lojatech/LojatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univale.lojatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
